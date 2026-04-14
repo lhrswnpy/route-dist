75c4ef93c5a915a1255e0fb9c32e29a7e288a8b6
lhrswnpy <wuweiwen537@outlook.com>
2026-04-14 16:06:39 +0800

    Add GitHub Actions workflow & fix vite base path for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/仓库名/',  // 注意：仓库名必须与你的 GitHub 仓库名称一致，以斜杠开头和结尾
+  base: '/personal-blog/',  // 注意：仓库名必须与你的 GitHub 仓库名称一致，以斜杠开头和结尾
 })
